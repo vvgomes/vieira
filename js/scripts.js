cd93f388b9f6c8197ef4d6674f64b602fe72b404
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2015-01-09 12:53:40 -0200

    Deployed from master: 9a64e8e

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  (function header() {
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
+
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      $('#js-navigation-menu').slideToggle(function(){
+        if($('#js-navigation-menu').is(':hidden')) {
+          $('#js-navigation-menu').removeAttr('style');
+        }
+      });
+    })
+  })();
+
+});
